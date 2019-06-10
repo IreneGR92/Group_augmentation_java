359038b1c816c2a0943f7b6ebe0c8efdfd748cc9
tobakk <17847628+tobakk@users.noreply.github.com>
2019-06-10 12:45:24 +0200

    typo

--- src/test/java/org/groupaugmentation/model/IndividualTest.java
+++ src/test/java/org/groupaugmentation/model/IndividualTest.java
@@ -10,7 +10,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class IndivudualTest {
+public class IndividualTest {
 
     private Map<GeneType, Double> genes;
 
